2612c444e69c5c148cea2cf1b5d80e61d431f9b7
Rinor Ajeti <r4ajeti@gmail.com>
2019-08-23 19:16:03 +0200

    FIXED: By TSE suggestion

--- Enumerable.rb
+++ Enumerable.rb
@@ -38,21 +38,53 @@ module Enumerable
         end
         bool
     end
-    def my_none?
+    def my_none?(*args)
         bool = true
         self.my_each do |x|
+        case args.length
+        when 0
             if !yield(x)
-                bool= false
+                bool = false
             end
+        when 1
+            if x==args.first
+                bool = false
+            end
+        else
+            raise ArgumentError
+        end
         end
         bool
+    end
+
+    def my_count(*args)
+        count = 0
+        case args.length
+        when 0
+            puts ' -- 0'
+            self.my_each do |x|
+                if yield(x)
+                    count = count + 1
+                end
+            end
+        when 1
+            puts ' -- 1 '
+            self.my_each do |x|
+                if args.first == x
+                    count = count + 1
+                end
+            end
+        else
+            raise ArgumentError
+        end
+        count
     end
 
     def my_count?
         count = 0
         self.my_each do |x|
             if yield(x)
-                count = count + 1
+                count += 1
             end
         end
         count
@@ -69,50 +101,57 @@ module Enumerable
     def my_inject (*item)
         acc = self[0]
         for x in self[1..self.length] do
-            acc=yield(acc, x)
+            acc = yield(acc, x)
         end
         return acc
     end
 end
 
 def multiply_els(list)
-    return list.my_inject(1) { |acc, nr| acc*nr}
+    return list.my_inject(1) { |acc, nr| acc*nr }
 end
 
-puts "Enumerate Methods: "
-puts "my_each method imp -> "
-[8,2,5,4].my_each{ |num| puts num }
+puts 'Enumerate Methods: '
+puts 'my_each method imp -> '
+[8, 2, 5, 4].my_each{ |num| puts num }
 
-puts "my_each_with_index -> "
-[8,2,5,4].my_each_with_index{ |num, ind| puts "Elem: #{num} and it's index: #{ind}" }
+puts 'my_each_with_index -> '
+[8, 2, 5, 4].my_each_with_index{ |num, ind| puts "Elem: #{num} and it's index: #{ind}" }
 
-puts "my_select method by using my_each -> "
+puts 'my_select method by using my_each -> '
 strings = %w(ruby html javascript)
-p ss = strings.my_select {|words| words.include?("r") }
+p ss = strings.my_select {|words| words.include?('r') }
 
-puts "my_all? method by using my_each -> "
-p ["ab","abc","abcdta"].my_all? { |x| x.length >= 2 }
+puts 'my_all? method by using my_each -> '
+p ['ab', 'abc', 'abcdta'].my_all? { |x| x.length >= 2 }
 
-puts "my_any? method by using my_each -> "
-p ["Ariel","Camus","Gaby","Willow","Lydia"].my_any? {|a| a.include?("y") }
+puts 'my_any? method by using my_each -> '
+p ['Ariel','Camus','Gaby','Willow','Lydia'].my_any? { |a| a.include?('y') }
 
-puts "my_none? method by using my_each -> "
-p ["ab","abc","abcdta"].my_none? { |x| x.length < 2 }
+puts 'my_none? method by using my_each -> without blocks '
+p ['ab','abc','abcdta'].my_none? { |x| x.length < 2 }
+puts 'my_none? method by using my_each -> with arguments'
+p [2, 5, 8, 6, 4, 1, 2, 3].my_none?(9)
 
-puts "my_count? method by using my_each -> "
-puts "How many numbers are even? "
-p [2,5,8,6,4,1,2,3].my_count?{ |x| x%2==0 }
-puts "How many words in array?"
-p ["ab","abc","abcdta"].my_count?{|x| true}
+puts 'my_count? method by using my_each -> '
+puts 'How many numbers are even? '
+p [2, 5, 8, 6, 4, 1, 2, 3].my_count?{ |x| x%2==0 }
+puts 'How many words in array?'
+p ['ab', 'abc', 'abcdta'].my_count?{ |x| true }
+puts 'How many 2 in array? using count?'
+p [2, 5, 8, 6, 4, 1, 2, 3].my_count?{ |x| x==2 }
 
-puts "my_map method by using my_each -> "
-puts "Double current values!"
-p [2,5,8,6,4,1,2,3].my_map { |num| num*2 }
+puts 'How many 2 in array? using count '
+p [2, 5, 8, 6, 4, 1, 2, 3].my_count(2)
 
-puts "multiply_els method using my_inject -> "
-var1 = multiply_els([2,4,5])
+puts 'my_map method by using my_each -> '
+puts 'Double current values!'
+p [2, 5, 8, 6, 4, 1, 2, 3].my_map { |num| num*2 }
+
+puts 'multiply_els method using my_inject -> '
+var1 = multiply_els([2, 4, 5])
 print var1
-puts ""
+puts ''
 
-puts "my_inject method -> "
-p [2,4,5].my_inject {|a,b| a*b}
+puts 'my_inject method -> '
+p [2, 4, 5].my_inject { |a,b| a*b }
